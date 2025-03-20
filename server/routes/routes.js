a26761fe43a7850fa3f8c0803504754aaad0b06f
Gabriel <Gabriel@DOMINIO.CLINICA>
2025-03-20 12:06:20 -0300

    First version back

---
+++ server/routes/routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const router = express.Router();
+const patientRoutes = require('./patientRoutes');
+const appointmentRoutes = require("./appointmentRoutes");
+const userRoutes = require("./userRoutes");
+const adminStatusRoutes = require("./administrativeStatusRoutes");
+const medicalStatusRoutes = require("./medicalStatusRoutes");
+
+router.use('/patients', patientRoutes);
+router.use('/appointments', appointmentRoutes);
+router.use("/users", userRoutes);
+router.use("/administrative-status", adminStatusRoutes);
+router.use("/medical-status", medicalStatusRoutes);
+
+module.exports = router;
